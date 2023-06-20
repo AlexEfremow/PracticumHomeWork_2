3d65cda725005146a52b36a84f1adeb5096e5102
Alex <alexsoutes@yandex.ru>
2023-06-20 16:27:01 +0300

    Commit_6

--- app/src/main/java/com/example/practicumhomework_2/SearchActivity.kt
+++ app/src/main/java/com/example/practicumhomework_2/SearchActivity.kt
@@ -1,19 +1,56 @@
 package com.example.practicumhomework_2
 
+import android.content.Context
 import android.os.Bundle
-import android.widget.Button
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 class SearchActivity : AppCompatActivity() {
+
+    private val editText by lazy { findViewById<EditText>(R.id.EditText) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search)
 
-        findViewById<Button>(R.id.return_button).setOnClickListener {
+        findViewById<ImageView>(R.id.return_button).setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+        val clearButton = findViewById<ImageView>(R.id.clear_button)
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                clearButton.isVisible = p0?.length != 0
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        }
+        editText.addTextChangedListener(textWatcher)
+        clearButton.setOnClickListener {
+            editText.text.clear()
+            inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("search_key", editText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restore = savedInstanceState.getString("search_key")
+        editText.setText(restore)
+    }
 }
 

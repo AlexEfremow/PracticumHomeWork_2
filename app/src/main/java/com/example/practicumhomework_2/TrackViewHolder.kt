b6d1228df9ddb18dabde77aff461e0df2be18f57
Alex <alexsoutes@yandex.ru>
2023-06-29 11:37:24 +0300

    Commit_7

---
+++ app/src/main/java/com/example/practicumhomework_2/TrackViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.practicumhomework_2
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    fun bind(model: Track) {
+    }
+}

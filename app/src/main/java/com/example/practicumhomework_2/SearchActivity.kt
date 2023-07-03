8fadad49b3fb48c2fa26939b0fb3ed3547ce03f1
Alex <alexsoutes@yandex.ru>
2023-07-03 18:16:13 +0300

    Search Track feature implemented

--- app/src/main/java/com/example/practicumhomework_2/SearchActivity.kt
+++ app/src/main/java/com/example/practicumhomework_2/SearchActivity.kt
@@ -9,6 +9,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchActivity : AppCompatActivity() {
 
@@ -17,6 +18,20 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        val trackList: MutableList<Track> = mutableListOf()
+        for (i in 0..4) {
+            trackList.add(
+                Track(
+                    SONG_NAMES[i], MUSICIAN_NAMES[i], TRACK_TIME[i], ARTWORK_URL[i]
+                )
+            )
+        }
+
+        val trackAdapter = TrackAdapter().also { it.updateTrackList(trackList) }
+        recyclerView.adapter = trackAdapter
 
         findViewById<ImageView>(R.id.return_button).setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
@@ -30,6 +45,10 @@ class SearchActivity : AppCompatActivity() {
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 clearButton.isVisible = p0?.length != 0
+                if (p0 != null) {
+                    val updatedList = trackList.filter { it.trackName.contains(p0) || it.artistName.contains(p0) }
+                    trackAdapter.updateTrackList(updatedList)
+                }
             }
 
             override fun afterTextChanged(p0: Editable?) {
@@ -51,6 +70,28 @@ class SearchActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         val restore = savedInstanceState.getString("search_key")
         editText.setText(restore)
+    }
+
+    companion object {
+        val SONG_NAMES = listOf(
+            "Smells Like Teen Spirit",
+            "Billie Jean",
+            "Stayin' Alive",
+            "Whole Lotta Love",
+            "Sweet Child O'Mine"
+        )
+        val MUSICIAN_NAMES =
+            listOf("Nirvana", "Michael Jackson", "Bee Gees", "Led Zeppelin", "Guns N' Roses")
+
+        val TRACK_TIME = listOf("5:01", "4:35", "4:10", "5:33", "5:03")
+
+        val ARTWORK_URL = listOf(
+            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg",
+            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        )
     }
 }
 
---
+++ app/src/main/java/com/example/practicumhomework_2/TrackViewHolder.kt
@@ -0,0 +1,6 @@
+package com.example.practicumhomework_2
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)

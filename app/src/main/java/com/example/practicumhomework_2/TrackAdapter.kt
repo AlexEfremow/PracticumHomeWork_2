7cdd85e25f4cce94fe8e7156f373150ba405e1ae
Alex <alexsoutes@yandex.ru>
2023-07-04 20:25:13 +0300

    Commit design changes

--- app/src/main/java/com/example/practicumhomework_2/TrackAdapter.kt
+++ app/src/main/java/com/example/practicumhomework_2/TrackAdapter.kt
@@ -6,14 +6,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackAdapter: RecyclerView.Adapter<TrackViewHolder>() {
-    private var tracks: List<Track> = emptyList()
+     var tracks: List<Track> = emptyList()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_item_layout, parent, false)
         return TrackViewHolder(view)
     }
-
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         val trackNameTextView = holder.itemView.findViewById<TextView>(R.id.track_name)
         val musicianNameTextView = holder.itemView.findViewById<TextView>(R.id.artist_name)
@@ -21,7 +22,12 @@ class TrackAdapter: RecyclerView.Adapter<TrackViewHolder>() {
         trackNameTextView.text = tracks[position].trackName
         musicianNameTextView.text = tracks[position].artistName
         trackTimeTextView.text = tracks[position].trackTime
-        Glide.with(holder.itemView.context).load(tracks[position].artworkUrl).into(holder.itemView.findViewById(R.id.image_url))
+        Glide.with(holder.itemView.context)
+            .load(tracks[position].artworkUrl)
+            .transform(CenterInside(), RoundedCorners(8))
+            .error(R.drawable.error)
+            .placeholder(R.drawable.error)
+            .into(holder.itemView.findViewById(R.id.image_url))
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/practicumhomework_2/TrackViewHolder.kt
+++ app/src/main/java/com/example/practicumhomework_2/TrackViewHolder.kt
@@ -1,6 +1,10 @@
 package com.example.practicumhomework_2
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)

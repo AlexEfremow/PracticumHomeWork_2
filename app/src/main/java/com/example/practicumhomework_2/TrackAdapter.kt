3b0de2ce417539135e2c5426f5e9fc0f2f5aa77f
Alex <alexsoutes@yandex.ru>
2023-07-06 21:30:45 +0300

    Commit 10 sprint

--- app/src/main/java/com/example/practicumhomework_2/TrackAdapter.kt
+++ app/src/main/java/com/example/practicumhomework_2/TrackAdapter.kt
@@ -9,30 +9,22 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class TrackAdapter: RecyclerView.Adapter<TrackViewHolder>() {
-     var tracks: List<Track> = emptyList()
+class TrackAdapter : RecyclerView.Adapter<TrackViewHolder>() {
+    var tracks: List<Track> = emptyList()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_item_layout, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.track_item_layout, parent, false)
         return TrackViewHolder(view)
     }
+
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-        val trackNameTextView = holder.itemView.findViewById<TextView>(R.id.track_name)
-        val musicianNameTextView = holder.itemView.findViewById<TextView>(R.id.artist_name)
-        val trackTimeTextView = holder.itemView.findViewById<TextView>(R.id.track_time)
-        trackNameTextView.text = tracks[position].trackName
-        musicianNameTextView.text = tracks[position].artistName
-        trackTimeTextView.text = tracks[position].trackTime
-        Glide.with(holder.itemView.context)
-            .load(tracks[position].artworkUrl)
-            .transform(CenterInside(), RoundedCorners(8))
-            .error(R.drawable.error)
-            .placeholder(R.drawable.error)
-            .into(holder.itemView.findViewById(R.id.image_url))
+        TrackViewHolder(holder.itemView).bind(tracks[position])
     }
 
     override fun getItemCount(): Int {
         return tracks.size
     }
+
     @SuppressLint("NotifyDataSetChanged")
     fun updateTrackList(trackList: List<Track>) {
         tracks = trackList
--- app/src/main/java/com/example/practicumhomework_2/TrackViewHolder.kt
+++ app/src/main/java/com/example/practicumhomework_2/TrackViewHolder.kt
@@ -7,4 +7,20 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    private val trackNameTextView = itemView.findViewById<TextView>(R.id.track_name)
+    private val musicianNameTextView = itemView.findViewById<TextView>(R.id.artist_name)
+    private val trackTimeTextView = itemView.findViewById<TextView>(R.id.track_time)
+
+    fun bind(track: Track) {
+        trackNameTextView.text = track.trackName
+        musicianNameTextView.text = track.artistName
+        trackTimeTextView.text = track.trackTime
+        Glide.with(itemView.context)
+            .load(track.artworkUrl)
+            .transform(CenterInside(), RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.track_cover_corner_radius)))// Не очень понял как использовать GetDimensionPixelSize, поэтому просто изменил значение на 2
+            .error(R.drawable.error)
+            .placeholder(R.drawable.placeholder)
+            .into(itemView.findViewById(R.id.image_url))
+    }
+}

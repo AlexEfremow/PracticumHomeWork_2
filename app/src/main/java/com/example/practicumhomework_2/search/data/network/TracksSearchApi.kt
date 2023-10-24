339ae140b01c7895ce523e282ed221e693625324
Alex <alexsoutes@yandex.ru>
2023-10-24 15:40:23 +0300

    Refactoring

--- app/src/main/java/com/example/practicumhomework_2/search/data/network/TracksSearchApi.kt
+++ app/src/main/java/com/example/practicumhomework_2/search/data/network/TracksSearchApi.kt
@@ -1,9 +1,6 @@
 package com.example.practicumhomework_2.search.data.network
 
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,12 +8,4 @@ interface TracksSearchApi {
 
     @GET("search?entity=song")
     fun searchTracks(@Query("term") value: String): Call<TrackSearchResponse>
-
-    companion object {
-        var retrofit = Retrofit.Builder()
-            .baseUrl("https://itunes.apple.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create<TracksSearchApi>()
-    }
 }
--- app/src/main/java/com/example/practicumhomework_2/search/di/module.kt
+++ app/src/main/java/com/example/practicumhomework_2/search/di/module.kt
@@ -4,8 +4,6 @@ import android.app.Application
 import android.content.SharedPreferences
 import com.example.practicumhomework_2.search.data.local.SearchPreferencesImpl
 import com.example.practicumhomework_2.search.data.network.TrackSearchWrapperImpl
-import com.example.practicumhomework_2.search.data.network.TracksSearchApi
-import com.example.practicumhomework_2.search.data.network.TracksSearchApi.Companion.retrofit
 import com.example.practicumhomework_2.search.domain.SearchInteractor
 import com.example.practicumhomework_2.search.domain.SearchPreferences
 import com.example.practicumhomework_2.search.domain.TrackSearchWrapper

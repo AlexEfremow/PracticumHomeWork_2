34fea4e84789a942d837f64e775cfd437c20bfc4
Alex <alexsoutes@yandex.ru>
2024-01-12 12:44:47 +0300

    PlaylistCreator UI

--- app/src/main/java/com/example/practicumhomework_2/media/presentation/MediaFragment.kt
+++ app/src/main/java/com/example/practicumhomework_2/media/presentation/MediaFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.practicumhomework_2.R
+import com.example.practicumhomework_2.databinding.MediaBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaFragment: Fragment() {
     lateinit var tabMediator: TabLayoutMediator
+    private var _binding: MediaBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,7 +36,6 @@ class MediaFragment: Fragment() {
             }
         }
         tabMediator.attach()
-
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/practicumhomework_2/media/presentation/PlaylistsFragment.kt
+++ app/src/main/java/com/example/practicumhomework_2/media/presentation/PlaylistsFragment.kt
@@ -1,21 +1,25 @@
 package com.example.practicumhomework_2.media.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.practicumhomework_2.R
+import com.example.practicumhomework_2.databinding.FragmentPlaylistsBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistsFragment : Fragment() {
     private val viewModel by viewModel<PlaylistsViewModel>()
+    private var _binding: FragmentPlaylistsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_playlists, container, false)
+    ): View {
+        _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

11f63763fe3658001500bff2c4e8890364d87785
Alex <alexsoutes@yandex.ru>
2024-01-12 13:27:34 +0300

    BottomNavigationView visibility fixed

--- app/src/main/java/com/example/practicumhomework_2/main/presentation/MainActivity.kt
+++ app/src/main/java/com/example/practicumhomework_2/main/presentation/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.practicumhomework_2.main.presentation
 import android.content.res.ColorStateList
 import android.content.res.XmlResourceParser
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.practicumhomework_2.R
@@ -11,15 +16,30 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private val bottomNavigationView by lazy { findViewById<BottomNavigationView>(R.id.bottomNavigationView) }
+    private val separator by lazy { findViewById<View>(R.id.separator) }
+
+    private val destinationListener = NavController.OnDestinationChangedListener { _, destination, _ ->
+        bottomNavigationView.isVisible = destination.id != R.id.playlistCreateFragment
+        separator.isVisible = destination.id != R.id.playlistCreateFragment
+    }
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navController.addOnDestinationChangedListener(destinationListener)
+
         bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        navController.removeOnDestinationChangedListener(destinationListener)
     }
 }

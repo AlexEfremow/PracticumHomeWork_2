5efec69fb92ca427949e14648a8fcac8fdd94d31
Alex <alexsoutes@yandex.ru>
2024-01-17 13:43:17 +0300

    PlaylistCover sizes

--- app/src/main/java/com/example/practicumhomework_2/media/presentation/PlaylistViewHolder.kt
+++ app/src/main/java/com/example/practicumhomework_2/media/presentation/PlaylistViewHolder.kt
@@ -12,15 +12,14 @@ import com.example.practicumhomework_2.media.domain.PlaylistModel
 class PlaylistViewHolder(private val binding: PlaylistItemBinding): RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: PlaylistModel) {
-        binding.playlistCover.setImageURI(Uri.parse(item.cover))
         binding.playlistName.text = item.name
         binding.playlistTracksCount.text = item.count.toString()
-//        Glide.with(binding.playlistCover)
-//            .load(item.cover)
-//            .transform(
-//                CenterCrop(),
-//                RoundedCorners(binding.root.context.resources.getDimensionPixelSize(R.dimen.track_cover_corner_radius))
-//            )
-//            .into(binding.playlistCover)
+        Glide.with(binding.playlistCover)
+            .load(item.cover)
+            .transform(
+                CenterCrop(),
+                RoundedCorners(50)
+            )
+            .into(binding.playlistCover)
     }
 }

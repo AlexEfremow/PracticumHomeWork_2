90b79de39339d363c0540c8b4ec3426ce3631e46
Alex <alexsoutes@yandex.ru>
2024-01-30 10:39:43 +0300

    Added PeekHeight for Bottom Sheet

--- app/src/main/java/com/example/practicumhomework_2/playlist/PlaylistFragment.kt
+++ app/src/main/java/com/example/practicumhomework_2/playlist/PlaylistFragment.kt
@@ -1,15 +1,25 @@
 package com.example.practicumhomework_2.playlist
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import androidx.fragment.app.Fragment
 import com.example.practicumhomework_2.databinding.PlaylistBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class PlaylistFragment: Fragment() {
     private var _binding : PlaylistBinding? = null
     private val binding get() = _binding!!
+    private val behavior by lazy { BottomSheetBehavior.from(binding.bottomView) }
+    private val screenHeight by lazy { Resources.getSystem().displayMetrics.heightPixels }
+    private val layoutListener = OnGlobalLayoutListener {
+        behavior.peekHeight = screenHeight - binding.topView.height
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +32,12 @@ class PlaylistFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.root.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.root.viewTreeObserver.removeOnGlobalLayoutListener(layoutListener)
     }
 
     override fun onDestroyView() {
